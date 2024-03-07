adbabe2af13cb6b9517b65debcf8a7e1115910d3
lowkey <i202420@nu.edu.pk>
2024-03-07 14:34:29 +0500

    added icons

--- src/sections/contactUs.jsx
+++ src/sections/contactUs.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import '../styles/contact.css';
 import '../styles/bootstrap.min.css'; 
+import EnvelopeIcon from 'bootstrap-icons/icons/envelope.svg';
+import TelegramIcon from 'bootstrap-icons/icons/telegram.svg';
+import GithubIcon from 'bootstrap-icons/icons/github.svg';
+import DiscordIcon from 'bootstrap-icons/icons/discord.svg';
 
 const ContactSection = () => {
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
@@ -44,26 +48,26 @@ const ContactSection = () => {
         <div className="row">
           <div className="col-lg-5 d-flex align-items-stretch">
             <div className="info">
-              <div className="email">
-                <i className="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>nabeeljaved944@gmail.com</p>
-              </div>
-              <i className="fa fa-telegram" style={{ fontSize: '36px', color: 'skyblue' }}></i>
-              <div className="message">
-                <h4>Telegram:</h4>
-                <p>@code4renawarden</p>
-              </div>
-              <div className="github">
-                <i className="bi bi-github"></i>
-                <h4>GitHub:</h4>
-                <p>@Nabeel-javaid</p>
-              </div>
-              <i className="bi bi-discord"></i>
-              <div className="Discord">
-                <h4>Discord:</h4>
-                <p>0xepley</p>
-              </div>
+            <div className="email">
+          <img src={EnvelopeIcon} alt="Email" style={{ width: '1.5em', height: '1.5em' }} />
+          <h4>Email:</h4>
+          <p>nabeeljaved944@gmail.com</p>
+        </div>
+        <div className="telegram">
+          <img src={TelegramIcon} alt="Telegram" style={{ width: '1.5em', height: '1.5em', color: 'skyblue' }} />
+          <h4>Telegram:</h4>
+          <p>@code4renawarden</p>
+        </div>
+        <div className="github">
+          <img src={GithubIcon} alt="GitHub" style={{ width: '1.5em', height: '1.5em' }} />
+          <h4>GitHub:</h4>
+          <p>@Nabeel-javaid</p>
+        </div>
+        <div className="discord">
+          <img src={DiscordIcon} alt="Discord" style={{ width: '1.5em', height: '1.5em' }} />
+          <h4>Discord:</h4>
+          <p>0xepley</p>
+        </div>
             </div>
           </div>
           <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
