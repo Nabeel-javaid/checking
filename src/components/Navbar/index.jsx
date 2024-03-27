7f25f801e92e244c66f7a99394360046012c4e74
lowkey <i202420@nu.edu.pk>
2024-03-27 14:01:18 +0500

    working code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -47,98 +47,96 @@ export default function Navbar() {
   }, []);
 
   return (
-    // <ThirdwebProvider
-    //   activeChain="goerli"
-    //   clientId="a3aade80daf0ec081979b0bb1ddf3f1c"
-    //   // locale={en()}
-    //   supportedWallets={[
-    //     metamaskWallet({ recommended: true }),
-    //     walletConnect(),
-    //     safeWallet({
-    //       personalWallets: [
-    //         metamaskWallet({ recommended: true }),
-    //         walletConnect(),
-    //         trustWallet(),
-    //         zerionWallet(),
-    //         bloctoWallet(),
-    //         frameWallet(),
-    //         rainbowWallet(),
-    //         phantomWallet(),
-    //       ],
-    //     }),
-    //     trustWallet(),
-    //     zerionWallet(),
-    //     bloctoWallet(),
-    //     frameWallet(),
-    //     rainbowWallet(),
-    //     phantomWallet(),
-    //   ]}
-    // >
-    //   <nav
-    //     className={classNames(
-    //       "fixed w-full transition-all duration-700 z-10 py-8",
-    //       { "bg-white shadow-lg !py-3": backgroundWhite }
-    //     )}
-    //   >
-    //     <div className="container top-0 flex items-center justify-between px-4 mx-auto">
-    //       <div className="flex items-center justify-center">
-    //         <img src={Logo} className="mr-6" alt="Neva" />
-    //       </div>
+    <ThirdwebProvider
+      activeChain="goerli"
+      clientId="a3aade80daf0ec081979b0bb1ddf3f1c"
+      // locale={en()}
+      supportedWallets={[
+        metamaskWallet({ recommended: true }),
+        walletConnect(),
+        safeWallet({
+          personalWallets: [
+            metamaskWallet({ recommended: true }),
+            walletConnect(),
+            trustWallet(),
+            zerionWallet(),
+            bloctoWallet(),
+            frameWallet(),
+            rainbowWallet(),
+            phantomWallet(),
+          ],
+        }),
+        trustWallet(),
+        zerionWallet(),
+        bloctoWallet(),
+        frameWallet(),
+        rainbowWallet(),
+        phantomWallet(),
+      ]}
+    >
+      <nav
+        className={classNames(
+          "fixed w-full transition-all duration-700 z-10 py-8",
+          { "bg-white shadow-lg !py-3": backgroundWhite }
+        )}
+      >
+        <div className="container top-0 flex items-center justify-between px-4 mx-auto">
+          <div className="flex items-center justify-center">
+            <img src={Logo} className="mr-6" alt="Neva" />
+          </div>
 
-    //       <div className="hidden gap-16 lg:flex">
-    //         <Menus />
-    //       </div>
+          <div className="hidden gap-16 lg:flex">
+            <Menus />
+          </div>
 
 
           
 
-    //       <div className="hidden gap-4 md:flex">
-    //         <ConnectWallet
-    //           theme={"dark"}
-    //           switchToActiveChain={true}
-    //           modalSize={"wide"}
-    //         />
-    //       </div>
+          <div className="hidden gap-4 md:flex">
+            <ConnectWallet
+              theme={"dark"}
+              switchToActiveChain={true}
+              modalSize={"wide"}
+            />
+          </div>
 
-    //       <div className="text-2xl md:hidden">
-    //         <button
-    //           className="z-50 block p-4 transition-all"
-    //           onClick={() => setDropdownOpen(!dropdownOpen)}
-    //         >
-    //           {dropdownOpen ? <BsX /> : <BsList />}
-    //         </button>
+          <div className="text-2xl md:hidden">
+            <button
+              className="z-50 block p-4 transition-all"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              {dropdownOpen ? <BsX /> : <BsList />}
+            </button>
 
-    //         <div
-    //           className={classNames({
-    //             "text-base left-0 top-full right-0 absolute transition-all duration-400": true,
-    //             "invisible opacity-0": !dropdownOpen,
-    //             "visible opacity-100": dropdownOpen,
-    //           })}
-    //         >
-    //           <div
-    //             className="left-0 h-screen bg-black bg-opacity-30"
-    //             onClick={handleWindowScroll}
-    //           >
-    //             <div className="z-20 p-6 bg-white shadow-xl">
-    //               <div className="flex gap-4 mb-6">
-    //                 <ConnectWallet
-    //                   theme={"black"}
-    //                   switchToActiveChain={true}
-    //                   modalSize={"wide"}
-    //                 />
-    //               </div>
-    //               <div className="mb-4">
-    //                 <Menus /> 
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </ThirdwebProvider>
-    <div>
-      <h2>Hello</h2>
-    </div>
+            <div
+              className={classNames({
+                "text-base left-0 top-full right-0 absolute transition-all duration-400": true,
+                "invisible opacity-0": !dropdownOpen,
+                "visible opacity-100": dropdownOpen,
+              })}
+            >
+              <div
+                className="left-0 h-screen bg-black bg-opacity-30"
+                onClick={handleWindowScroll}
+              >
+                <div className="z-20 p-6 bg-white shadow-xl">
+                  <div className="flex gap-4 mb-6">
+                    <ConnectWallet
+                      theme={"black"}
+                      switchToActiveChain={true}
+                      modalSize={"wide"}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <Menus /> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </ThirdwebProvider>
+    
   );
 }
