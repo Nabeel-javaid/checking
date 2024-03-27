d105444dad86e89c603aa14075c34ab1e9cd5b17
Zaryabk9 <i202487@nu.edu.pk>
2024-03-28 03:19:07 +0500

    Profile Page owner name fixed

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Grid, Typography, Paper, Button, Button as MUIButton, List, ListItem, ListItemText, ListItemIcon, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
-import Layout from '../../components/Layout';
-import LinkIcon from '@mui/icons-material/Link';
-import { createClient } from '@supabase/supabase-js';
-import StorefrontIcon from '@mui/icons-material/Storefront';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  Button as MUIButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import Layout from "../../components/Layout";
+import LinkIcon from "@mui/icons-material/Link";
+import { createClient } from "@supabase/supabase-js";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 import contractABI from "../../ABIs/marketRegistery.json";
 import { ethers } from "ethers";
-import ScaleLoader from 'react-spinners/ScaleLoader';
+import ScaleLoader from "react-spinners/ScaleLoader";
 import { Alchemy, Network } from "alchemy-sdk";
-import Footer from "../../Footer"
-import "../Profile/profile.css"
+import Footer from "../../Footer";
+import "../../pages/Profile/profile.css";
 
+const supabaseUrl = "https://lmsbzqlwsedldqxqwzlv.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxtc2J6cWx3c2VkbGRxeHF3emx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5ODA2MTEsImV4cCI6MjAxMzU1NjYxMX0.-qVOdECSW9hfokq8N99gCH2BZYpWooXy7zOz1e6fBHM";
 
-const supabaseUrl = "https://lmsbzqlwsedldqxqwzlv.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxtc2J6cWx3c2VkbGRxeHF3emx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5ODA2MTEsImV4cCI6MjAxMzU1NjYxMX0.-qVOdECSW9hfokq8N99gCH2BZYpWooXy7zOz1e6fBHM"
-
-
-const etherscanApiKey = 'UXWX37Y2Y4PQM8ZZ1XQ2KBWH6AVZ8J7RYU';
+const etherscanApiKey = "UXWX37Y2Y4PQM8ZZ1XQ2KBWH6AVZ8J7RYU";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const UserProfile = () => {
-  const [walletAddress, setWalletAddress] = useState('');
+  const [walletAddress, setWalletAddress] = useState("");
   const [transactions, setTransactions] = useState([]);
   const [openMarkets, setOpenMarkets] = useState([]);
   const [closedMarkets, setClosedMarkets] = useState([]);
-  const [ownerAddress, setOwnerAddress] = useState('');
+  const [ownerAddress, setOwnerAddress] = useState("");
   const [cancelDialogOpen, setCancelDialogOpen] = useState(false);
-  const [MID, setMID] = useState('');
+  const [MID, setMID] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const contractAddress = '0xad9ace8a1ea7267dc2ab19bf4b10465d56d5ecf0';
+  const contractAddress = "0xad9ace8a1ea7267dc2ab19bf4b10465d56d5ecf0";
   const provider = new ethers.providers.Web3Provider(window.ethereum);
 
   async function closeMarket(marketID) {
@@ -39,13 +53,17 @@ const UserProfile = () => {
   }
 
   async function isMarketOpen(marketID) {
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
+    const contract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
 
     try {
       const isOpen = await contract.isMarketOpen(marketID);
       return isOpen;
     } catch (error) {
-      console.error('Error calling isMarketOpen:', error.message);
+      console.error("Error calling isMarketOpen:", error.message);
       return false;
     }
   }
@@ -53,41 +71,41 @@ const UserProfile = () => {
   async function fetchMarkets() {
     try {
       const { data, error } = await supabase
-        .from('Markets')
+        .from("Markets")
         .select("*")
-        .ilike('owner', walletAddress);
+        .ilike("owner", walletAddress);
 
       if (error) {
         throw new Error(`Error fetching markets: ${error.message}`);
       }
 
       if (data) {
-        console.log("data from supabase")
-        const markets = data.map(async market => {
+        console.log("data from supabase");
+        const markets = data.map(async (market) => {
           const isOpen = await isMarketOpen(market.id);
           return {
             ...market,
-            isOpen
+            isOpen,
           };
         });
 
-        Promise.all(markets).then(updatedMarkets => {
-          const open = updatedMarkets.filter(market => market.isOpen);
-          const closed = updatedMarkets.filter(market => !market.isOpen);
+        Promise.all(markets).then((updatedMarkets) => {
+          const open = updatedMarkets.filter((market) => market.isOpen);
+          const closed = updatedMarkets.filter((market) => !market.isOpen);
 
           setOpenMarkets(open);
           setClosedMarkets(closed);
         });
       }
     } catch (error) {
-      console.error('Error fetching markets:', error.message);
+      console.error("Error fetching markets:", error.message);
     }
   }
 
   function getWalletAddress() {
     const address = window.ethereum.selectedAddress;
-    console.log('Wallet address:', address);
-    console.log('Wallet address:', address);
+    console.log("Wallet address:", address);
+    console.log("Wallet address:", address);
     setWalletAddress(address);
   }
 
@@ -99,25 +117,28 @@ const UserProfile = () => {
         if (!walletAddress) {
           return;
         }
-         checkENS(walletAddress);
+        checkENS(walletAddress);
 
-        const response = await fetch(`https://api-goerli.etherscan.io/api?module=account&action=txlist&address=${walletAddress}&startblock=0&endblock=99999999&sort=desc&apikey=${etherscanApiKey}`);
+        const response = await fetch(
+          `https://api-goerli.etherscan.io/api?module=account&action=txlist&address=${walletAddress}&startblock=0&endblock=99999999&sort=desc&apikey=${etherscanApiKey}`
+        );
         const data = await response.json();
 
         if (data.result) {
-          const truncatedTransactions = data.result.slice(0, 3).map(transaction => ({
-            ...transaction,
-            hash: `${transaction.hash.substring(0, 30)}...`
-          }));
+          const truncatedTransactions = data.result
+            .slice(0, 3)
+            .map((transaction) => ({
+              ...transaction,
+              hash: `${transaction.hash.substring(0, 30)}...`,
+            }));
           setTransactions(truncatedTransactions);
         }
-
 
         if (supabase && walletAddress) {
           await fetchMarkets();
         }
       } catch (error) {
-        console.error('Error fetching data:', error.message);
+        console.error("Error fetching data:", error.message);
       }
     };
 
@@ -126,10 +147,8 @@ const UserProfile = () => {
 
   useEffect(() => {
     // Set the background color when the component mounts
-    document.body.style.backgroundColor = 'rgba(246,249,255,1)';
-
+    document.body.style.backgroundColor = "rgba(246,249,255,1)";
   }, []);
-
 
   async function checkENS(walletAddressToCheck) {
     try {
@@ -138,14 +157,13 @@ const UserProfile = () => {
         network: Network.ETH_MAINNET,
       };
       const alchemy = new Alchemy(config);
-      
+
       const ensContractAddress = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85";
       const nfts = await alchemy.nft.getNftsForOwner(walletAddressToCheck, {
         contractAddresses: [ensContractAddress],
       });
-      
-      console.log("nfts", nfts.ownedNfts[0].name);
 
+      console.log("nfts", nfts.ownedNfts[0].name);
 
       if (nfts) {
         setOwnerAddress(nfts.ownedNfts[0].name);
@@ -167,53 +185,76 @@ const UserProfile = () => {
       // setLoading(true);
 
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(contractAddress, contractABI, signer);
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
 
       const txResponse = await contract.closeMarket(MID);
 
       await txResponse.wait();
-      console.log('Transaction hash:', txResponse.hash);
-      console.log('Transaction confirmed in block:', txResponse.blockNumber);
+      console.log("Transaction hash:", txResponse.hash);
+      console.log("Transaction confirmed in block:", txResponse.blockNumber);
 
       const { error } = await supabase
-        .from('Markets')
+        .from("Markets")
         .update({ isClosed: true })
-        .eq('id', MID);
+        .eq("id", MID);
 
       fetchMarkets();
     } catch (error) {
-      console.error('Error calling closeMarket:', error.message);
+      console.error("Error calling closeMarket:", error.message);
       // You might want to show an error message to the user or handle it appropriately
     } finally {
       setLoading(false);
     }
   };
 
-
-
-
   return (
     <Layout>
-      <Container style={{ paddingTop: '9rem', paddingBottom: '7rem', backgroundColor: 'rgba(246,249,255,1)' }}>
+      <Container
+        style={{
+          paddingTop: "9rem",
+          paddingBottom: "7rem",
+          backgroundColor: "rgba(246,249,255,1)",
+        }}
+      >
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Paper elevation={6} style={{ position: 'relative', padding: '2rem', borderRadius: '16px', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
+            <Paper
+              elevation={6}
+              style={{
+                position: "relative",
+                padding: "2rem",
+                borderRadius: "16px",
+                background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+              }}
+            >
               <img
                 src="https://i.ibb.co/DL3dtSj/avatar2-0.png"
                 alt="User"
                 style={{
-                  position: 'absolute',
-                  top: '2.5rem',
-                  left: '63rem',
-                  width: '100px',
-                  height: '100px'
+                  position: "absolute",
+                  top: "2.5rem",
+                  left: "63rem",
+                  width: "100px",
+                  height: "100px",
+                  
                 }}
               />
-              <Typography variant="h6" className='owner' style={{ color: 'white', marginBottom: '1rem' }}>
-                <strong>Owner: {ownerAddress || walletAddress}</strong>
+              <Typography
+                variant="h6"
+                
+                style={{ color: "white", marginBottom: "1rem" }}
+              >
+                <strong className="ownername">Owner: {ownerAddress || walletAddress}</strong>
               </Typography>
 
-              <Typography variant="body1" style={{ marginBottom: '1rem', color: 'white' }}>
+              <Typography
+                variant="body1"
+                style={{ marginBottom: "1rem", color: "white" }}
+              >
                 <strong>Network:</strong> <strong>Ethereum</strong>
               </Typography>
 
@@ -231,7 +272,15 @@ const UserProfile = () => {
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <Paper elevation={6} style={{ padding: '2rem', borderRadius: '16px', background: '#f0f0f0', minHeight: '100%' }}>
+            <Paper
+              elevation={6}
+              style={{
+                padding: "2rem",
+                borderRadius: "16px",
+                background: "#f0f0f0",
+                minHeight: "100%",
+              }}
+            >
               <Typography variant="h5">Previous Transactions</Typography>
               <List>
                 {transactions.map((transaction, index) => (
@@ -257,9 +306,17 @@ const UserProfile = () => {
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <Paper elevation={6} style={{ padding: '2rem', borderRadius: '16px', background: '#f0f0f0', minHeight: '100%' }}>
+            <Paper
+              elevation={6}
+              style={{
+                padding: "2rem",
+                borderRadius: "16px",
+                background: "#f0f0f0",
+                minHeight: "100%",
+              }}
+            >
               <Typography variant="h5">Loans</Typography>
-              <ul style={{ paddingLeft: '1rem', marginTop: '1rem' }}>
+              <ul style={{ paddingLeft: "1rem", marginTop: "1rem" }}>
                 {/* {loans.map((loan, index) => (
                   <li key={index}>{loan}</li>
                 ))} */}
@@ -268,16 +325,27 @@ const UserProfile = () => {
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <Paper elevation={6} style={{ padding: '2rem', borderRadius: '16px', background: '#f0f0f0', minHeight: '100%' }}>
+            <Paper
+              elevation={6}
+              style={{
+                padding: "2rem",
+                borderRadius: "16px",
+                background: "#f0f0f0",
+                minHeight: "100%",
+              }}
+            >
               <Typography variant="h5">Open Markets</Typography>
-              <ul style={{ paddingLeft: '1rem', marginTop: '1rem' }}>
+              <ul style={{ paddingLeft: "1rem", marginTop: "1rem" }}>
                 {openMarkets.map((market, index) => (
                   <div key={index}>
                     <ListItem>
                       <ListItemIcon>
-                        <StorefrontIcon style={{ marginRight: '0.5rem' }} />
+                        <StorefrontIcon style={{ marginRight: "0.5rem" }} />
                       </ListItemIcon>
-                      <ListItemText primary={market.name} secondary={market.description} />
+                      <ListItemText
+                        primary={market.name}
+                        secondary={market.description}
+                      />
                       <Button
                         variant="outlined"
                         color="secondary"
@@ -286,7 +354,13 @@ const UserProfile = () => {
                         Close Market
                       </Button>
                     </ListItem>
-                    <hr style={{ borderTop: '3px solid #ccc', marginTop: '1rem', marginBottom: '1rem' }} />
+                    <hr
+                      style={{
+                        borderTop: "3px solid #ccc",
+                        marginTop: "1rem",
+                        marginBottom: "1rem",
+                      }}
+                    />
                   </div>
                 ))}
               </ul>
@@ -294,18 +368,35 @@ const UserProfile = () => {
           </Grid>
 
           <Grid item xs={12} md={6}>
-            <Paper elevation={6} style={{ padding: '2rem', borderRadius: '16px', background: '#f0f0f0', minHeight: '100%' }}>
+            <Paper
+              elevation={6}
+              style={{
+                padding: "2rem",
+                borderRadius: "16px",
+                background: "#f0f0f0",
+                minHeight: "100%",
+              }}
+            >
               <Typography variant="h5">Closed Markets</Typography>
-              <ul style={{ paddingLeft: '1rem', marginTop: '1rem' }}>
+              <ul style={{ paddingLeft: "1rem", marginTop: "1rem" }}>
                 {closedMarkets.map((market, index) => (
                   <div key={index}>
                     <ListItem>
                       <ListItemIcon>
-                        <StorefrontIcon style={{ marginRight: '0.5rem' }} />
+                        <StorefrontIcon style={{ marginRight: "0.5rem" }} />
                       </ListItemIcon>
-                      <ListItemText primary={market.name} secondary={market.description} />
+                      <ListItemText
+                        primary={market.name}
+                        secondary={market.description}
+                      />
                     </ListItem>
-                    <hr style={{ borderTop: '3px solid #ccc', marginTop: '1rem', marginBottom: '1rem' }} />
+                    <hr
+                      style={{
+                        borderTop: "3px solid #ccc",
+                        marginTop: "1rem",
+                        marginBottom: "1rem",
+                      }}
+                    />
                   </div>
                 ))}
               </ul>
@@ -315,41 +406,49 @@ const UserProfile = () => {
           <Dialog open={cancelDialogOpen} onClose={handleCancelCancel}>
             <DialogTitle>Warning</DialogTitle>
             <DialogContent>
-              <p>Market Closing is irreversible and trading cannot happen in closed markets, do you still want to close your market?</p>
+              <p>
+                Market Closing is irreversible and trading cannot happen in
+                closed markets, do you still want to close your market?
+              </p>
             </DialogContent>
             <DialogActions>
               <MUIButton onClick={handleCancelCancel} color="primary">
                 No
               </MUIButton>
-              <MUIButton onClick={() => {
-                handleCancelConfirm(MID);
-                setCancelDialogOpen(false);
-                setLoading(true);  
-              }} color="primary">
+              <MUIButton
+                onClick={() => {
+                  handleCancelConfirm(MID);
+                  setCancelDialogOpen(false);
+                  setLoading(true);
+                }}
+                color="primary"
+              >
                 Yes
               </MUIButton>
             </DialogActions>
           </Dialog>
 
           {loading && (
-            <div style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              backgroundColor: 'transparent', // Semi-transparent white background
-              zIndex: 9999,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
+            <div
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "transparent", // Semi-transparent white background
+                zIndex: 9999,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <ScaleLoader color={"#123abc"} loading={loading} size={22} />
             </div>
           )}
         </Grid>
       </Container>
-      <Footer/>
+      <Footer />
     </Layout>
   );
 };
